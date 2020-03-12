15bb8dbd33bf11f7bc0372051be2215a8addbc8c
Takayuki Maeda <maeda.takayuki@moneyforward.co.jp>
2020-03-13 00:19:22 +0900

    Update sqlhandler.go

--- db/database_sql/infrastructure/sqlhandler.go
+++ db/database_sql/infrastructure/sqlhandler.go
@@ -11,7 +11,7 @@ type SqlHandler struct {
 }
 
 func NewSqlHandler() *SqlHandler {
-	conn, err := sql.Open("mysql", "root:@tcp(db:3306)/sample")
+	conn, err := sql.Open("mysql", "root:password@tcp(localhost:3306)/golang_study")
 	if err != nil {
 		panic(err.Error)
 	}
