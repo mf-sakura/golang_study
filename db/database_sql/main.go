7053816f801f4fc4c31ad499c70f3197f6c730c0
Takayuki Maeda <maeda.takayuki@moneyforward.co.jp>
2020-03-13 01:17:54 +0900

    remove useless handlder

--- db/database_sql/main.go
+++ db/database_sql/main.go
@@ -2,8 +2,6 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo/v4"
 	"github.com/mf-sakura/golang_study/db/database_sql/infrastructure"
 	"github.com/mf-sakura/golang_study/db/database_sql/interfaces/controllers"
@@ -14,14 +12,9 @@ func main() {
 	sqlHandler := infrastructure.NewSqlHandler()
 	userController := controllers.NewUserController(sqlHandler.Conn)
 
-	e.GET("/user", helloHandler)
 	e.GET("/users", func(c echo.Context) error { return userController.Index(c) })
 	e.GET("/users/:id", func(c echo.Context) error { return userController.Show(c) })
 	e.POST("/users", func(c echo.Context) error { return userController.Create(c) })
 
 	e.Logger.Fatal(e.Start(":8080"))
-}
-
-func helloHandler(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello World from Go.")
 }
