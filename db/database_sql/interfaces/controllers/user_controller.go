5167ad9c49c622aa6f102d665cc4f08733abb3f1
Takayuki Maeda <maeda.takayuki@moneyforward.co.jp>
2020-03-13 00:19:25 +0900

    Create user_controller.go

---
+++ db/database_sql/interfaces/controllers/user_controller.go
@@ -0,0 +1,50 @@
+package controllers
+
+import (
+	"database/sql"
+	"fmt"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+
+	"github.com/mf-sakura/golang_study/db/database_sql/interfaces/database"
+
+	"github.com/mf-sakura/golang_study/db/database_sql/domain"
+)
+
+type UserController struct {
+	db *sql.DB
+}
+
+func NewUserController(db *sql.DB) *UserController {
+	return &UserController{db: db}
+}
+
+func (controller *UserController) Create(c echo.Context) error {
+	u := domain.User{}
+	c.Bind(&u)
+	id, err := database.Store(controller.db, u)
+	if err != nil {
+		return c.JSON(500, err)
+	}
+	return c.JSON(201, id)
+}
+
+func (controller *UserController) Index(c echo.Context) error {
+	fmt.Println(controller.db)
+	users, err := database.FindAll(controller.db)
+	if err != nil {
+		fmt.Println(err)
+		return c.JSON(500, err)
+	}
+	return c.JSON(200, users)
+}
+
+func (controller *UserController) Show(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	user, err := database.FindById(controller.db, id)
+	if err != nil {
+		return c.JSON(500, err)
+	}
+	return c.JSON(200, user)
+}
