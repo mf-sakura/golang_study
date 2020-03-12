00642c895202b8077e4ac2d0378c020a61265893
Takayuki Maeda <maeda.takayuki@moneyforward.co.jp>
2020-03-12 12:15:32 +0900

    Create main.go

---
+++ db/database_sql/main.go
@@ -0,0 +1,72 @@
+// メイン関数(実行時に呼ばれる関数)を含むpackageはmainにする
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+)
+
+var (
+	counter = 0
+)
+
+func main() {
+	e := echo.New()
+
+	// GET
+	e.GET("/user", helloHandler)
+	// GET 200以外のStatus
+	e.POST("/user", unAuthorizedHandler)
+	// GET Headerの読み込み
+	e.PUT("/user", squareHandler)
+	// POST Bodyの読み込み
+	e.POST("/user", incrementHandler)
+
+	// 8080ポートで起動
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+// レスポンスに`Hello World`を書き込むハンドラー
+// 引数をこの形にするのはechoの仕様から決まっている
+func helloHandler(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello World from Go.")
+}
+
+// 200以外のHTTP Statusを返すハンドラー
+func unAuthorizedHandler(c echo.Context) error {
+	return echo.NewHTTPError(http.StatusUnauthorized, "UnAuthorized")
+}
+
+// Headerから数字を取得して、その二乗を返すハンドラー
+func squareHandler(c echo.Context) error {
+	// Headerの読み込み
+	numStr := c.Request().Header.Get("num")
+	// String -> Intの変換
+	num, err := strconv.Atoi(numStr)
+	if err != nil {
+		// 他のエラーの可能性もあるがサンプルとして纏める
+		return echo.NewHTTPError(http.StatusBadRequest, "num is not integer")
+	}
+	// fmt.Sprintfでフォーマットに沿った文字列を生成できる。
+	return c.String(http.StatusOK, fmt.Sprintf("Square of %d is equal to %d", num, num*num))
+}
+
+// Bodyから数字を取得してその数字だけCounterをIncrementするハンドラー
+// DBがまだないので簡易的なもの
+func incrementHandler(c echo.Context) error {
+	incrRequest := incrRequest{}
+	if err := c.Bind(&incrRequest); err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "Internal Server Error")
+	}
+	counter += incrRequest.Num
+	return c.String(http.StatusOK, fmt.Sprintf("Value of Counter is %d \n", counter))
+}
+
+type incrRequest struct {
+	// jsonタグをつける事でjsonのunmarshalが出来る
+	// jsonパッケージに渡すので、Publicである必要がある
+	Num int `json:"num"`
+}
