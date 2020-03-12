7cae4cf7ae945c814bdc5ba467f54f3d157e92de
Takayuki Maeda <maeda.takayuki@moneyforward.co.jp>
2020-03-13 00:20:05 +0900

    remove handlers

--- db/database_sql/main.go
+++ db/database_sql/main.go
@@ -2,71 +2,26 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"strconv"
 
 	"github.com/labstack/echo/v4"
-)
-
-var (
-	counter = 0
+	"github.com/mf-sakura/golang_study/db/database_sql/infrastructure"
+	"github.com/mf-sakura/golang_study/db/database_sql/interfaces/controllers"
 )
 
 func main() {
 	e := echo.New()
+	sqlHandler := infrastructure.NewSqlHandler()
+	userController := controllers.NewUserController(sqlHandler.Conn)
 
-	// GET
 	e.GET("/user", helloHandler)
-	// GET 200以外のStatus
-	e.POST("/user", unAuthorizedHandler)
-	// GET Headerの読み込み
-	e.PUT("/user", squareHandler)
-	// POST Bodyの読み込み
-	e.POST("/user", incrementHandler)
+	e.GET("/users", func(c echo.Context) error { return userController.Index(c) })
+	e.GET("/users/:id", func(c echo.Context) error { return userController.Show(c) })
+	e.POST("/users", func(c echo.Context) error { return userController.Create(c) })
 
-	// 8080ポートで起動
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
-// レスポンスに`Hello World`を書き込むハンドラー
-// 引数をこの形にするのはechoの仕様から決まっている
 func helloHandler(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello World from Go.")
-}
-
-// 200以外のHTTP Statusを返すハンドラー
-func unAuthorizedHandler(c echo.Context) error {
-	return echo.NewHTTPError(http.StatusUnauthorized, "UnAuthorized")
-}
-
-// Headerから数字を取得して、その二乗を返すハンドラー
-func squareHandler(c echo.Context) error {
-	// Headerの読み込み
-	numStr := c.Request().Header.Get("num")
-	// String -> Intの変換
-	num, err := strconv.Atoi(numStr)
-	if err != nil {
-		// 他のエラーの可能性もあるがサンプルとして纏める
-		return echo.NewHTTPError(http.StatusBadRequest, "num is not integer")
-	}
-	// fmt.Sprintfでフォーマットに沿った文字列を生成できる。
-	return c.String(http.StatusOK, fmt.Sprintf("Square of %d is equal to %d", num, num*num))
-}
-
-// Bodyから数字を取得してその数字だけCounterをIncrementするハンドラー
-// DBがまだないので簡易的なもの
-func incrementHandler(c echo.Context) error {
-	incrRequest := incrRequest{}
-	if err := c.Bind(&incrRequest); err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, "Internal Server Error")
-	}
-	counter += incrRequest.Num
-	return c.String(http.StatusOK, fmt.Sprintf("Value of Counter is %d \n", counter))
-}
-
-type incrRequest struct {
-	// jsonタグをつける事でjsonのunmarshalが出来る
-	// jsonパッケージに渡すので、Publicである必要がある
-	Num int `json:"num"`
 }
