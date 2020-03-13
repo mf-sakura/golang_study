c8eeb346a1dd4b5e2413737c13b93272553326b8
Takayuki Maeda <maeda.takayuki@moneyforward.co.jp>
2020-03-13 20:29:25 +0900

    Update main.go

--- db/database_sql/main.go
+++ db/database_sql/main.go
@@ -1,19 +1,55 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
+	"flag"
+	"fmt"
+	"log"
+
 	"github.com/mf-sakura/golang_study/db/database_sql/infrastructure"
 	"github.com/mf-sakura/golang_study/db/database_sql/interfaces/controllers"
 )
 
 func main() {
-	e := echo.New()
 	sqlHandler := infrastructure.NewSqlHandler()
 	userController := controllers.NewUserController(sqlHandler.Conn)
+	option := flag.String("a", "-h", "action")
+	id := flag.String("i", "1", "user id")
+	firstName := flag.String("f", "Alan", "first name")
+	lastName := flag.String("l", "Turing", "last name")
+	flag.Parse()
 
-	e.GET("/users", func(c echo.Context) error { return userController.Index(c) })
-	e.GET("/users/:id", func(c echo.Context) error { return userController.Show(c) })
-	e.POST("/users", func(c echo.Context) error { return userController.Create(c) })
-
-	e.Logger.Fatal(e.Start(":8080"))
+	switch *option {
+	case "index":
+		users, err := userController.Index()
+		if err != nil {
+			log.Println(err)
+		}
+		for _, user := range users {
+			fmt.Printf("ID: %v, FirstName: %v, LastName: %v\n", user.ID, user.FirstName, user.LastName)
+		}
+		return
+	case "show":
+		if *id == "" {
+			log.Fatal("You need a user.id.")
+		}
+		user, err := userController.Show(*id)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("ID: %v, FirstName: %v, LastName: %v\n", user.ID, user.FirstName, user.LastName)
+		return
+	case "create":
+		if *firstName == "" {
+			log.Fatal("You need a first name.")
+		}
+		if *lastName == "" {
+			log.Fatal("You need a last name.")
+		}
+		user, err := userController.Create(*firstName, *lastName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("ID: %v, FirstName: %v, LastName: %v\n", user.ID, user.FirstName, user.LastName)
+		return
+	}
 }
