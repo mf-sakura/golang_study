0355383755b15505b85ff92f55a9ffc7a76a7830
Takayuki Maeda <41065217+TaKO8Ki@users.noreply.github.com>
2020-03-19 19:22:24 +0900

    Create main.go

---
+++ db/sqlx/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/mf-sakura/golang_study/db/sqlx/infrastructure"
+	"github.com/mf-sakura/golang_study/db/sqlx/interfaces/controllers"
+)
+
+func main() {
+	sqlHandler := infrastructure.NewSqlHandler()
+	userController := controllers.NewUserController(sqlHandler.Conn)
+	option := flag.String("a", "-h", "action")
+	id := flag.String("i", "", "user id")
+	firstName := flag.String("f", "Alan", "first name")
+	lastName := flag.String("l", "Turing", "last name")
+	flag.Parse()
+
+	switch *option {
+	case "index":
+		users, err := userController.Index()
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, user := range users {
+			fmt.Printf("ID: %v, FirstName: %v, LastName: %v\n", user.ID, user.FirstName, user.LastName)
+		}
+		return
+	case "show":
+		if *id == "" {
+			log.Fatal("You need a user.id.")
+		}
+		user, err := userController.Show(*id)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("ID: %v, FirstName: %v, LastName: %v\n", user.ID, user.FirstName, user.LastName)
+		return
+	case "create":
+		if *firstName == "" {
+			log.Fatal("You need a first name.")
+		}
+		if *lastName == "" {
+			log.Fatal("You need a last name.")
+		}
+		user, err := userController.Create(*firstName, *lastName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("ID: %v, FirstName: %v, LastName: %v\n", user.ID, user.FirstName, user.LastName)
+		return
+	}
+}
