e852d8182b0765bc82d0a12d4daa752110ed11fe
alpaca-tc <alpaca-tc@alpaca.tc>
2020-03-26 18:13:35 +0900

    2. `incrementHandler`でPOST以外のMethodの場合に、405エラーを返してください。

--- http_server/net_http/main.go
+++ http_server/net_http/main.go
@@ -69,6 +69,11 @@ func squareHandler(w http.ResponseWriter, req *http.Request) {
 // Bodyから数字を取得してその数字だけCounterをIncrementするハンドラー
 // DBがまだないので簡易的なもの
 func incrementHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
 	body := req.Body
 	// bodyの読み込みに開いたio Readerを最後にCloseする
 	defer body.Close()
