5bffdd811523cf5704f6e4605e640cd3ca5e27b2
Hiroaki Otsu <ootsuhiroaki@gmail.com>
2020-03-30 00:29:27 +0900

    task 1

--- http_server/echo/main.go
+++ http_server/echo/main.go
@@ -50,6 +50,9 @@ func squareHandler(c echo.Context) error {
 		// 他のエラーの可能性もあるがサンプルとして纏める
 		return echo.NewHTTPError(http.StatusBadRequest, "num is not integer")
 	}
+	if num > 100 {
+		return echo.NewHTTPError(http.StatusBadRequest, "num over 100")
+	}
 	// fmt.Sprintfでフォーマットに沿った文字列を生成できる。
 	return c.String(http.StatusOK, fmt.Sprintf("Square of %d is equal to %d", num, num*num))
 }
