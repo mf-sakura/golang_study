7fc9b33cd9e2d797ab74d9b3cfb3b99f18bfa416
mfst2018 <snow.taku@moneyforward.co.jp>
2020-03-30 16:35:26 +0900

    net/http 課題の 1
    squareHandlerでnumが100以上の場合はバリデーションエラーとして、400エラーを返してください。

--- http_server/net_http/main.go
+++ http_server/net_http/main.go
@@ -52,6 +52,11 @@ func squareHandler(w http.ResponseWriter, req *http.Request) {
 		fmt.Fprint(w, "num is not integer")
 		return
 	}
+	if num >= 100 {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "num is 100 or more")
+		return
+	}
 	// fmt.Sprintfでフォーマットに沿った文字列を生成できる。
 	fmt.Fprint(w, fmt.Sprintf("Square of %d is equal to %d", num, num*num))
 }
