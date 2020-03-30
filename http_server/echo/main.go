6d34b4d21b51e21398ccd3b33169fa9d72f0e67c
takashima <nobutakaman@gmail.com>
2020-03-30 15:43:37 +0900

    課題1 実装

--- http_server/echo/main.go
+++ http_server/echo/main.go
@@ -50,6 +50,11 @@ func squareHandler(c echo.Context) error {
 		// 他のエラーの可能性もあるがサンプルとして纏める
 		return echo.NewHTTPError(http.StatusBadRequest, "num is not integer")
 	}
+	// validation
+	// FIXME これ使いたい https://github.com/go-playground/validator
+	if num >= 100 {
+		return echo.NewHTTPError(http.StatusBadRequest, "num must be less than 100.")
+	}
 	// fmt.Sprintfでフォーマットに沿った文字列を生成できる。
 	return c.String(http.StatusOK, fmt.Sprintf("Square of %d is equal to %d", num, num*num))
 }
