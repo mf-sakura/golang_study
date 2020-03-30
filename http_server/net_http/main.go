fc88c64e152c0b0559aa0c7a1964e90ff7e2a35d
mfst2018 <snow.taku@moneyforward.co.jp>
2020-03-30 16:36:23 +0900

    net/http 課題の 2
    incrementHandlerでPOST以外のMethodの場合に、405エラーを返してください。

--- http_server/net_http/main.go
+++ http_server/net_http/main.go
@@ -64,6 +64,11 @@ func squareHandler(w http.ResponseWriter, req *http.Request) {
 // Bodyから数字を取得してその数字だけCounterをIncrementするハンドラー
 // DBがまだないので簡易的なもの
 func incrementHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprint(w, "Method is not POST")
+		return
+	}
 	body := req.Body
 	// bodyの読み込みに開いたio Readerを最後にCloseする
 	defer body.Close()
