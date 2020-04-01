f4f5971c7ec9db6215701d00fb9cdc923515dd36
luccafort <konishi.tatsuro@moneyforward.co.jp>
2020-04-02 00:25:01 +0900

    第4回mapの課題
    期待された実装にはなっていないがソートはできているのでこれで出す。
    正解は第5回で確認する

--- syntax/map/main.go
+++ syntax/map/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	// mapが参照型である事の確認
@@ -15,26 +18,38 @@ func main() {
 	// ---------
 	// mapに対するfor
 	// 学籍番号と学生名のMap
-	studnetIDMap := map[int]string{
+	studentIDMap := map[int]string{
 		3: "田中",
 		1: "伊藤",
 		2: "佐藤",
 		4: "佐々木",
 	}
 
-	for k, v := range studnetIDMap {
+	for _, k := range sortByKeys(studentIDMap) {
 		// fmt.Printfでフォーマットに従った文字列を標準出力に出せる
-		fmt.Printf("Name of StudentID:%d is %s\n", k, v)
+		fmt.Printf("Name of StudentID:%d is %s\n", k, studentIDMap[k])
 	}
 }
 
 func addMap(m map[string]string) {
 	m["a"] = "あ"
 }
+
 func addMapWithCopy(m map[string]string) {
 	copied := make(map[string]string)
 	for k, v := range m {
 		copied[k] = v
 	}
 	copied["i"] = "い"
+}
+
+func sortByKeys(m map[int]string) (keys []int) {
+	for k := range m {
+		keys = append(keys, k)
+	}
+	// 期待されたソートの仕方ではないがsort.Sliceの第一引数に
+	// mapだったものをどうsliceにするか？が解決できなかった
+	sort.Ints(keys)
+	//fmt.Println(keys)
+	return
 }
