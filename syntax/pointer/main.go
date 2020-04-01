f732959617de14c360d1c3536429b15efbbbbb1c
ookura keisuke <ookura.keisuke@moneyforward.co.jp>
2020-04-01 23:58:03 +0900

    pointerの課題

--- syntax/pointer/main.go
+++ syntax/pointer/main.go
@@ -6,6 +6,7 @@ import (
 
 func main() {
 	n := 100
+	fmt.Printf("origin n's pointer is %v\n", &n)
 	// 値渡し
 	// コピーされるので、元のnに変化はない
 	returnValue := increment(n)
@@ -18,8 +19,10 @@ func main() {
 }
 
 func increment(n int) int {
+	fmt.Printf("n's pointer is %v in increment\n", &n)
 	return n + 1
 }
 func incrementWithPointer(n *int) {
+	fmt.Printf("n's pointer is %v in incrementWithPointer\n", n)
 	*n++
 }
