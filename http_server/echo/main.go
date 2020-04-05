e2a5739e9b29a7af50ad55452a0d25a0b655c099
toru taniguchi <peta.tt@gmail.com>
2020-04-05 21:04:10 +0900

    typo

--- http_server/echo/main.go
+++ http_server/echo/main.go
@@ -96,8 +96,8 @@ func randomHandHandler(c echo.Context) error {
 	suits := []string{"s", "h", "d", "c"}
 	numbers := []string{"2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"}
 
-	card1 := choice(numbers + choice(suits)
-	card2 := choice(numbers + choice(suits)
+	card1 := choice(numbers) + choice(suits)
+	card2 := choice(numbers) + choice(suits)
 	hand := card1 + card2
 
 	return c.String(http.StatusOK, fmt.Sprintf("your hand is %s. raise or fold?", hand))
