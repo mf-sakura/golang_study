a1794b4c4b9d79b723301b48c4a0aff2179ae584
kamillle <32205171+kamillle@users.noreply.github.com>
2020-04-05 22:07:03 +0900

    Map課題

---
+++ syntax/map/go.mod
@@ -0,0 +1,3 @@
+module github.com/kamillle/golang_study/syntax/map
+
+go 1.14
--- syntax/map/main.go
+++ syntax/map/main.go
@@ -1,30 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	// mapが参照型である事の確認
+	// map == hash, map[KeyType]ValueType
+	// nilを入れたmapを生成することはできるが、書き込みができない(panicが起こる)ので、初期化したい時はmakeを使う
+	// makeで初期化できるのはmap, slice, channel
 	m := make(map[string]string)
 	// main関数内でmは変更していないが参照渡しされるので変更される。
 	addMap(m)
-	fmt.Println(m)
+	fmt.Printf("After addMap is %v\n", m)
 	// copyしているので、mに影響はない
 	addMapWithCopy(m)
-	fmt.Println(m)
+	fmt.Printf("After addMapWithCopy is %v\n", m)
 
 	// ---------
 	// mapに対するfor
 	// 学籍番号と学生名のMap
-	studnetIDMap := map[int]string{
+	studentIdMap := map[int]string{
 		3: "田中",
 		1: "伊藤",
 		2: "佐藤",
 		4: "佐々木",
 	}
 
-	for k, v := range studnetIDMap {
-		// fmt.Printfでフォーマットに従った文字列を標準出力に出せる
-		fmt.Printf("Name of StudentID:%d is %s\n", k, v)
+	// sliceを定義して、ここにkeyを入れていく
+	studentIds := []int{}
+	for key := range studentIdMap {
+		studentIds = append(studentIds, key)
+	}
+	sort.Ints(studentIds)
+
+	for _, k := range studentIds {
+		fmt.Printf("Name of StudentID:%d is %s\n", k, studentIdMap[k])
 	}
 }
 
