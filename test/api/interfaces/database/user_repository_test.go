5eb47c3e48c62c84bfa38776549da6226077779b
Takayuki Maeda <41065217+TaKO8Ki@users.noreply.github.com>
2020-04-08 03:06:24 +0900

    Create user_repository_test.go

---
+++ test/api/interfaces/database/user_repository_test.go
@@ -0,0 +1,102 @@
+package database
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+	"testing"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"github.com/mf-sakura/golang_study/test/api/domain"
+	"github.com/mf-sakura/golang_study/test/api/infrastructure"
+)
+
+var sqlHandler infrastructure.SQLHandler
+
+func TestMain(m *testing.M) {
+	runTests := m.Run()
+	os.Exit(runTests)
+}
+
+func GetTestTransaction() *sqlx.Tx {
+	conn, err := sqlx.Open("mysql", "root:rootpassword@tcp(127.0.0.1:3314)/golang_study_test")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	err = conn.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	sqlHandler.Conn = conn
+	tx := conn.MustBegin()
+
+	return tx
+}
+
+func TestStore(t *testing.T) {
+	type args struct {
+		db *sqlx.DB
+		u  domain.User
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "正常系",
+			args: args{
+				u: domain.User{
+					FirstName: "hogehoge",
+					LastName:  "hogeohgoehgoehoh",
+				},
+			},
+			wantErr: false,
+			// Execをかをモックして異常系のテストしたいけどinterface使わないとしんどい
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tx := GetTestTransaction()
+			fmt.Println("hfoawhef;oawj")
+			_, err := Store(tx, tt.args.u)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Store() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			tx.Rollback()
+		})
+	}
+}
+
+// 課題にするメソッド
+// gotestsでの自動生成やってみる
+func TestFirstNameLike(t *testing.T) {
+	type args struct {
+		db        *sqlx.DB
+		firstName string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    domain.Users
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := FirstNameLike(tt.args.db, tt.args.firstName)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FirstNameLike() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("FirstNameLike() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
