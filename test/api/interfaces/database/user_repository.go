db5cc751dec21ab7e83a3e303a0632a7325c3bea
Takayuki Maeda <41065217+TaKO8Ki@users.noreply.github.com>
2020-04-08 03:06:19 +0900

    Update user_repository.go

--- test/api/interfaces/database/user_repository.go
+++ test/api/interfaces/database/user_repository.go
@@ -7,13 +7,14 @@ import (
 )
 
 // Store is a function for creating a user.
-func Store(db *sqlx.DB, u domain.User) (int, error) {
+func Store(tx *sqlx.Tx, u domain.User) (int, error) {
 	// prepared statement
 	// SQL Injection対策
-	stmt, err := db.Prepare("INSERT INTO users (first_name, last_name) VALUES (?,?)")
+	stmt, err := tx.Prepare("INSERT INTO users (first_name, last_name) VALUES (?,?)")
 	if err != nil {
 		return 0, err
 	}
+
 	// 関数終了時にstatementをcloseする
 	defer stmt.Close()
 	// SQL文実行
@@ -34,6 +35,16 @@ func FindAll(db *sqlx.DB) (domain.Users, error) {
 	var users []domain.User
 	// https://godoc.org/github.com/jmoiron/sqlx#DB.Select
 	if err := db.Select(&users, "SELECT id, first_name, last_name FROM users"); err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+// FirstNameLike is a function for getting all users whose first name is like ?.
+func FirstNameLike(db *sqlx.DB, firstName string) (domain.Users, error) {
+	var users []domain.User
+	// https://godoc.org/github.com/jmoiron/sqlx#DB.Select
+	if err := db.Select(&users, "SELECT id, first_name, last_name FROM users WHERE first_name LIKE ?", "%"+firstName+"%"); err != nil {
 		return nil, err
 	}
 	return users, nil
