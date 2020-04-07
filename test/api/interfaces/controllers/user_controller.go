e7694e20933dd60fc2999f4ea96f26cd94e3fe3a
Takayuki Maeda <41065217+TaKO8Ki@users.noreply.github.com>
2020-04-08 03:06:17 +0900

    Update user_controller.go

--- test/api/interfaces/controllers/user_controller.go
+++ test/api/interfaces/controllers/user_controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"database/sql"
 	"net/http"
 
 	"github.com/jmoiron/sqlx"
@@ -21,21 +22,41 @@ func NewUserController(db *sqlx.DB) *UserController {
 }
 
 // Create is a function for creating a user.
-func (controller *UserController) Create(c echo.Context) error {
+func (controller *UserController) Create(c echo.Context) (err error) {
 	user := domain.User{}
 	if err := c.Bind(&user); err != nil {
 		return err
 	}
-	id, err := database.Store(controller.db, user)
+	tx := controller.db.MustBegin()
+	defer func() {
+		if rollbackErr := tx.Rollback(); rollbackErr != nil {
+			// tx が閉じていることにより
+			// rollback には失敗するが特に問題はないのでエラーは返さない
+			if rollbackErr != sql.ErrTxDone {
+				err = rollbackErr
+			}
+		}
+	}()
+	id, err := database.Store(tx, user)
 	if err != nil {
-		return err
+		return
 	}
 	user.ID = id
+	tx.Commit()
 	return c.JSON(http.StatusCreated, &user)
 }
 
 // Index is a function for returning all users.
 func (controller *UserController) Index(c echo.Context) error {
+	firstName := c.QueryParam("first_name")
+	if firstName != "" {
+		users, err := database.FirstNameLike(controller.db, firstName)
+		if err != nil {
+			return err
+		}
+		return c.JSON(http.StatusOK, &users)
+	}
+
 	users, err := database.FindAll(controller.db)
 	if err != nil {
 		return err
