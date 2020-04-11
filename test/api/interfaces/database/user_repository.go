eda395fcebc5d3176f91d51c79e2668f2aff5d4f
luccafort <konishi.tatsuro@moneyforward.co.jp>
2020-04-11 14:51:31 +0900

    第7回の課題提出
    FirstNameLikeに対するテストコードを実装
    テスト実行前にデータを登録する処理の実装方法はよくわからなかったのでSQLで直接データを登録して検証しました。
    DeepEqualでIDまで対象に含まれてしまうのが微妙

--- test/api/interfaces/database/user_repository.go
+++ test/api/interfaces/database/user_repository.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"fmt"
+
 	"github.com/jmoiron/sqlx"
 
 	"github.com/mf-sakura/golang_study/test/api/domain"
@@ -43,6 +45,8 @@ func FindAll(db *sqlx.DB) (domain.Users, error) {
 // FirstNameLike is a function for getting all users whose first name is like ?.
 func FirstNameLike(db *sqlx.DB, firstName string) (domain.Users, error) {
 	var users []domain.User
+
+	fmt.Printf("SELECT id, first_name, last_name FROM users WHERE first_name LIKE %s", "%"+firstName+"%")
 	// https://godoc.org/github.com/jmoiron/sqlx#DB.Select
 	if err := db.Select(&users, "SELECT id, first_name, last_name FROM users WHERE first_name LIKE ?", "%"+firstName+"%"); err != nil {
 		return nil, err
--- test/api/interfaces/database/user_repository_test.go
+++ test/api/interfaces/database/user_repository_test.go
@@ -79,18 +79,45 @@ func TestFirstNameLike(t *testing.T) {
 		want    domain.Users
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name:    "正常系: FirstNameに「太郎」が含まれるあいまい検索ができる",
+			args:    args{firstName: "太郎"},
+			want:    domain.Users{{ID: 1, FirstName: "一太郎", LastName: "兼進"}},
+			wantErr: false,
+		},
+		{
+			name:    "正常系: FirstNameに「郎」が含まれるあいまい検索ができる",
+			args:    args{firstName: "郎"},
+			want:    domain.Users{{ID: 1, FirstName: "一太郎", LastName: "兼進"}, {ID: 2, FirstName: "次郎坊", LastName: "兼進"}},
+			wantErr: false,
+		},
+		{
+			name:    "正常系: FirstName「花子」に該当するデータがないのであいまい検索できるが空配列が返る",
+			args:    args{firstName: "花子"},
+			want:    nil,
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			tx := GetTestTransaction()
+			// INSERT INTO `users` (first_name, last_name) VALUES ("一太郎", "兼進")
+			// INSERT INTO `users` (first_name, last_name) VALUES ("次郎坊", "兼進")
+			// 前提となるSeedデータの登録の仕方がわからない
+			// 上記SQL相当のことがしたいだけなのだが
+			// 今回はやりかたがわからなかったのでmysqlに入って上記のSQLを実行してからテストを行っています
 			got, err := FirstNameLike(db, tt.args.firstName)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("FirstNameLike() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
+
+			// 比較でIDまで比較されてしまうのがテストとして微妙。
+			// 登録されたデータだけが確認できるようにしたいが……
 			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("FirstNameLike() = %v, want %v", got, tt.want)
+				t.Errorf("FirstNameLike() = %+v, want %+v", got, tt.want)
 			}
+			tx.Rollback()
 		})
 	}
 }
