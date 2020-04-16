58276c30bc8b3bb1264df6a18073e0c0b1b07b92
ookura keisuke <ookura.keisuke@moneyforward.co.jp>
2020-04-16 22:34:15 +0900

    課題2

--- interface/api/interfaces/controllers/user_controller_test.go
+++ interface/api/interfaces/controllers/user_controller_test.go
@@ -21,6 +21,7 @@ var (
 	mockID          = 2
 	userJSON        = `{"first_name":"John ","last_name":"Doe"}`
 	invalidUserJSON = `{"first_name": 1,"last_name": 2}`
+	noNameUserJSON  = `{"first_name": "","last_name": ""}`
 )
 
 func (r *mockUserRepository) Store(db *sqlx.DB, u domain.User) (int, error) {
@@ -44,6 +45,13 @@ func newUserContext() echo.Context {
 func newInvalidUserContext() echo.Context {
 	e := echo.New()
 	invalidReq := httptest.NewRequest(http.MethodPost, "/users", strings.NewReader(invalidUserJSON))
+	invalidReq.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	return e.NewContext(invalidReq, httptest.NewRecorder())
+}
+
+func noNameUserContext() echo.Context {
+	e := echo.New()
+	invalidReq := httptest.NewRequest(http.MethodPost, "/users", strings.NewReader(noNameUserJSON))
 	invalidReq.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
 	return e.NewContext(invalidReq, httptest.NewRecorder())
 }
@@ -89,6 +97,15 @@ func TestUserController_Create(t *testing.T) {
 				repository: &mockUserRepository{},
 			},
 			args:    args{c: newInvalidUserContext()},
+			wantErr: true,
+		},
+		{
+			name: "ユーザー名が空",
+			fields: fields{
+				db:         nil,
+				repository: &mockUserRepository{},
+			},
+			args:    args{c: noNameUserContext()},
 			wantErr: true,
 		},
 	}
