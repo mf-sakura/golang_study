15b05d2042babf11f43fa46459ff89e686c8e124
Takayuki Maeda <41065217+TaKO8Ki@users.noreply.github.com>
2020-04-17 23:15:10 +0900

    Update client.rb

--- grpc/ruby_client/client.rb
+++ grpc/ruby_client/client.rb
@@ -1,12 +1,13 @@
 require './proto/helloworld_services_pb.rb'
 require './proto/notification_services_pb.rb'
 
-hello_client = Sample::Greeter::Stub.new('127.0.0.1:5502', :this_channel_is_insecure)
-hello_request = Sample::HelloRequest.new(first_name: 'Alan', last_name: 'Turing')
-hello_response = hello_client.say_hello(hello_request)
-puts "Message: #{hello_response.message}"
+HOST = '127.0.0.1:5502'
 
-notification_client = Sample::Notifier::Stub.new('127.0.0.1:5502', :this_channel_is_insecure) 
+hello_client = Sample::Greeter::Stub.new(HOST, :this_channel_is_insecure)
+hello_request = Sample::HelloRequest.new(first_name: 'Alan', last_name: 'Turing')
+puts "Message: #{hello_client.say_hello(hello_request).message}"
+
+notification_client = Sample::Notifier::Stub.new(HOST, :this_channel_is_insecure) 
 notification_request = Sample::HelloRequest.new(first_name: 'Alan', last_name: 'Turing')
 notification_resopnses = notification_client.pereodic_hello(notification_request)
 notification_resopnses.each do |response|
