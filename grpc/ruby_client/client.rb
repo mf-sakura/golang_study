e1e9dedfe1aa079b3461996fce48494814edd105
luccafort <konishi.tatsuro@moneyforward.co.jp>
2020-04-20 01:09:48 +0900

    Streamな通信を出力する実装を追加
    Streamなレスポンスを受け取ったときにどうRubyで扱うのかわからなかったのでチュートリアルで実装を参考にした
    https://github.com/grpc/grpc.io/blob/master/content/docs/tutorials/basic/ruby.md

--- grpc/ruby_client/client.rb
+++ grpc/ruby_client/client.rb
@@ -2,12 +2,26 @@
 
 require 'grpc'
 require './proto/helloworld_services_pb'
+require './proto/notification_services_pb'
 
-client = Sample::Greeter::Stub.new(
+# 通常のレスポンス
+http_client = Sample::Greeter::Stub.new(
   '127.0.0.1:5502',
   :this_channel_is_insecure
 )
 
-opts = { first_name: 'John', last_name: 'Doe' }
-response = client.say_hello(Sample::HelloRequest.new(opts))
-puts response
+request_opts = Sample::HelloRequest.new(first_name: 'John', last_name: 'Doe')
+http_response = http_client.say_hello(request_opts)
+puts http_response
+
+puts '====================='
+
+# Streamなレスポンスを受け取ったとき
+stream_client = Sample::Notifier::Stub.new(
+  '127.0.0.1:5502',
+  :this_channel_is_insecure
+)
+
+request_opts = Sample::HelloRequest.new(first_name: '太郎', last_name: '兼進')
+stream_request = stream_client.pereodic_hello(request_opts)
+stream_request.each { |req| puts req }
