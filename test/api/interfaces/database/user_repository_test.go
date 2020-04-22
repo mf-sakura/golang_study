21e0fb3dd1335b3cffb6dadfd7eeba80e067a6c4
toru taniguchi <peta.tt@gmail.com>
2020-04-22 22:36:58 +0900

    firstnameのあいまい検索のテストを追加する

--- test/api/interfaces/database/user_repository_test.go
+++ test/api/interfaces/database/user_repository_test.go
@@ -7,11 +7,15 @@ import (
 	"testing"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-testfixtures/testfixtures/v3"
 	"github.com/jmoiron/sqlx"
 	"github.com/mf-sakura/golang_study/test/api/domain"
 )
 
-var db *sqlx.DB
+var (
+	db       *sqlx.DB
+	fixtures *testfixtures.Loader
+)
 
 func TestMain(m *testing.M) {
 	conn, err := sqlx.Open("mysql", "root:rootpassword@tcp(127.0.0.1:3314)/golang_study_test")
@@ -24,8 +28,25 @@ func TestMain(m *testing.M) {
 		panic(err)
 	}
 	db = conn
+
+	fixtures, err = testfixtures.New(
+		testfixtures.Database(db.DB),
+		testfixtures.Dialect("mysql"),
+		testfixtures.Directory("../../testdata/fixtures"),
+	)
+	if err != nil {
+		panic(err)
+	}
+
 	runTests := m.Run()
 	os.Exit(runTests)
+}
+
+func prepareTestDB() {
+	fmt.Printf("%v", fixtures)
+	if err := fixtures.Load(); err != nil {
+		panic(err)
+	}
 }
 
 func GetTestTransaction() *sqlx.Tx {
@@ -79,8 +100,22 @@ func TestFirstNameLike(t *testing.T) {
 		want    domain.Users
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name:    "正常系1",
+			args:    args{firstName: "田"},
+			want:    domain.Users{{ID: 1, FirstName: "田中", LastName: "一郎"}},
+			wantErr: false,
+		},
+		{
+			name:    "正常系2",
+			args:    args{firstName: "鈴"},
+			want:    domain.Users{{ID: 2, FirstName: "鈴木", LastName: "二郎"}},
+			wantErr: false,
+		},
 	}
+
+	prepareTestDB()
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := FirstNameLike(db, tt.args.firstName)
