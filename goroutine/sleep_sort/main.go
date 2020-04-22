e9803340156e2ddfb332b720f4191d60de71089b
ookura keisuke <ookura.keisuke@moneyforward.co.jp>
2020-04-22 22:43:06 +0900

    課題

--- goroutine/sleep_sort/main.go
+++ goroutine/sleep_sort/main.go
@@ -16,22 +16,24 @@ func main() {
 	// Channelは別の回でやるので、Goroutineだけで頑張る例
 	sortedNums := make([]int, 0, len(nums))
 
+	var wg sync.WaitGroup
+
 	for _, num := range nums {
-		// goroutineでは無名関数も使える
-		// ここでnumを渡す事で、forが進んでも各Goroutineのスコープ内でnは変化しない。
+		wg.Add(1)
+
 		go func(n int) {
-			// n秒スリープする
 			time.Sleep(time.Duration(n) * time.Second)
-			// mutexのLock
-			// 他のGoroutineからのアクセスがブロックされる
 			mu.Lock()
-			// deferで必ずUnlockする
-			defer mu.Unlock()
+
+			defer func() {
+				mu.Unlock()
+				wg.Done()
+			}()
 			sortedNums = append(sortedNums, n)
 		}(num) // (num)が無名関数の引数
 	}
 
-	time.Sleep(6 * time.Second)
+	wg.Wait()
 	fmt.Println(sortedNums)
 
 }
