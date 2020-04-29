cd7adb7cf954d443c95355e241e8bc1937ffe010
luccafort <konishi.tatsuro@moneyforward.co.jp>
2020-04-30 01:06:12 +0900

    第11回課題2の解答
    受信専用チャンネルを使ってSleepSortを実装
    受信専用チャンネルではclose(channel)できなかったので関数を2つにわけて実装する苦肉の策
    SleepSortを行う関数を実装、WaitGroupで処理の同期を実装（でも引数にチャンネル取るのがダサい）

--- channel/task/sleep_sort/main.go
+++ channel/task/sleep_sort/main.go
@@ -1,4 +1,45 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// 受信専用channel, for range, close←を使ってSleepSortを実装する
 func main() {
+	nums := []int{1, 3, 2, 5, 4}
+	sortedNums := []int{}
+
+	for v := range receivedChan(nums) {
+		fmt.Println(v)
+		sortedNums = append(sortedNums, v)
+	}
+	fmt.Println(sortedNums)
+}
+
+func receivedChan(nums []int) <-chan int {
+	count := len(nums)
+	ch := make(chan int, count)
+	go func(nums []int) {
+		defer close(ch)
+		sleepSortCh(ch, nums)
+	}(nums)
+
+	return ch
+}
+
+func sleepSortCh(ch chan int, nums []int) chan int {
+	var wg sync.WaitGroup
+	for _, num := range nums {
+		wg.Add(1)
+		go func(num int) {
+			defer wg.Done()
+			time.Sleep(time.Duration(num) * time.Second)
+			ch <- num
+		}(num)
+	}
+	wg.Wait()
+
+	return ch
 }
